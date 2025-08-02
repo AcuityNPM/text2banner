2b140e342a61bff2aacc95017bc5f35d8dd4093a
Bananabb <samson8682@gmail.com>
2025-08-02 08:40:29 +0800

    ADD: project init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # text2banner
 This project is a Strapi v5 plugin that provides functionality to generate a simple banner using customizable text and color codes.
+
+### Manually Installation
+1. Move to `plugins` folder `WORKDIR/src/plugins/`.
+2. Use Git Pull the source code to the `plugins` folder.
+
+### Initial Setup
+1. Put below source code to `WORKDIR/config/plugins.ts`
+```
+export default () => ({
+	'text2banner': {
+		enabled: true,
+		resolve: './src/plugins/text2banner'
+	},
+});
+```
+2. Then move to plugin folder `cd WORKDIR/src/plugins/text2banner`
+3. Please use the following command to set up the plugin:
+```
+npm install
+npm run build
+```
---
+++ server/src/controllers/controller.ts
@@ -0,0 +1,73 @@
+import type { Core } from '@strapi/strapi';
+import { errors } from '@strapi/utils';
+import { createCanvas } from 'canvas';
+import * as fs from 'fs';
+
+const controller = ({ strapi }: { strapi: Core.Strapi }) => ({
+  async generateBanner(ctx) {
+    try {
+      const post = ctx.request.body;
+      const canvas = createCanvas(post.bannerWidth, post.bannerHeight);
+      const context = canvas.getContext('2d');
+      const numNext = post.title.split('\n').length;
+            numNext = (numNext > 0) ? numNext - 1 : 0;
+
+      context.fillStyle = post.backgroundColor;
+      context.fillRect(0, 0, post.bannerWidth, post.bannerHeight);
+
+      context.fillStyle = post.fontColor;
+      context.font = post.fontSize + 'px Menlo';
+      context.textAlign = 'center';
+      context.fillText(post.title, (post.bannerWidth - (numNext * post.fontSize)) / 2, (post.bannerHeight - (numNext * post.fontSize)) / 2);
+
+
+      const rootDir = process.cwd();
+      const genFileName = post.bannerName + '.jpg';
+      const genFilePath = `${rootDir}/public/uploads/${genFileName}`
+      const file = await fs.writeFile(
+        genFilePath,
+        canvas.toBuffer('image/jpeg'),
+        (err: any) => {
+          if (err) {
+            throw new errors.ApplicationError(err);
+            return;
+          }
+          return 'File Saved!';
+        }
+      );
+
+      //uploading it directly to upload services.
+      const uploadResponse = await strapi.plugins.upload.services.upload.upload({
+        data:{},
+        files: {
+          filepath: genFilePath,
+          originalFilename: genFileName,
+          mimetype: 'image/jpeg',
+        }, 
+      });
+
+      // Remove the source file
+      const remove = await fs.unlink(genFilePath, (err: any) => {
+        if (err) {
+          throw new errors.ApplicationError(err);
+          return;
+        }
+        return 'Successfully removed!';
+      });
+
+      ctx.body = "ok";
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  
+  index(ctx) {
+    ctx.body = strapi
+      .plugin('text2banner')
+      // the name of the service file & the method.
+      .service('service')
+      .getWelcomeMessage();
+  },
+});
+
+export default controller;
