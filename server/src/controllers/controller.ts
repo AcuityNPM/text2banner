67c78c3470eed968ddc0448f4a7f9130d298f95f
Bananabb <samson8682@gmail.com>
2025-08-03 00:59:22 +0800

    ADD: multi-line function

--- server/src/controllers/controller.ts
+++ server/src/controllers/controller.ts
@@ -9,18 +9,31 @@ const controller = ({ strapi }: { strapi: Core.Strapi }) => ({
       const post = ctx.request.body;
       const canvas = createCanvas(post.bannerWidth, post.bannerHeight);
       const context = canvas.getContext('2d');
-      let numNext = post.title.split('\n').length;
-          numNext = (numNext > 0) ? numNext - 1 : 0;
 
+      // Background color of banner
       context.fillStyle = post.backgroundColor;
       context.fillRect(0, 0, post.bannerWidth, post.bannerHeight);
 
-      context.fillStyle = post.fontColor;
-      context.font = post.fontSize + 'px Menlo';
-      context.textAlign = 'center';
-      context.fillText(post.title, (post.bannerWidth - (numNext * post.fontSize)) / 2, (post.bannerHeight - (numNext * post.fontSize)) / 2);
+      // Fill the text to banner
+      if (!post.title) {
+        let textArr = post.title.split('\n');
+        let numNext = textArr.length;
+            numNext = (numNext > 0) ? numNext - 1 : 0;
+        // For Text line-height
+        let textHig = post.fontSize * 1.2;
+        context.fillStyle = post.fontColor;
+        context.font = post.fontSize + 'px Menlo';
+        context.textAlign = 'center';
+        for (let i = 0; i <= numNext; i++) {
+          if (i == 0) {
+            context.fillText(textArr[i], post.bannerWidth / 2, (post.bannerHeight - (numNext * textHig)) / 2);
+          } else {
+            context.fillText(textArr[i], post.bannerWidth / 2, (post.bannerHeight - ((numNext - (i * 2)) * textHig)) / 2);
+          }
+        }
+      }
 
-
+      // Create a banner and temp to store in folder
       const rootDir = process.cwd();
       const genFileName = post.bannerName + '.jpg';
       const genFilePath = `${rootDir}/public/uploads/${genFileName}`
@@ -36,7 +49,7 @@ const controller = ({ strapi }: { strapi: Core.Strapi }) => ({
         }
       );
 
-      //uploading it directly to upload services.
+      // Uploading it directly to upload services.
       const uploadResponse = await strapi.plugins.upload.services.upload.upload({
         data:{},
         files: {
